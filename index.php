a1b39fe19186f5b20a1ba769d0e89eb368e4c270
briteenrisen <100101733+briteenrisen@users.noreply.github.com>
2022-03-08 17:06:29 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,69 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE HTML>
+<html>
+<title>Self Posting Form practice. </title>
 
 <head>
-    <title>Brite Pedigree Registry</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .error {
+            color: #FF0000;
+        }
+    </style>
 </head>
 
 <body>
 
-
     <?php
-
-    $userNameErr = $passwordErr = "";
-    $userName = $password = "";
+    // define variables and set to empty values
+    $nameErr = $emailErr = $genderErr = $websiteErr = "";
+    $name = $email = $gender = $comment = $website = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["userName"])) {
-            $userNameErr = "Username Required";
+        //retrieves all data passed into the url. and accesses each by the given name in the html code
+        if (empty($_POST["name"])) {
+            $nameErr = "Name is required";
         } else {
-            $userName = test_input($_POST["userName"]);
+            $name = test_input($_POST["name"]);
 
-            if (!preg_match("/^[a-zA-Z-' ]*$/", $userName)) {
-                $userNameErr = "Only letters and white space allowed";
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+                $nameErr = "Only letters and white space allowed";
             }
         }
 
-        if (empty($_POST["password"])) {
-            $passwordErr = "Password Required";
+        if (empty($_POST["email"])) {
+            $emailErr = "Email is required";
         } else {
-            $password = test_input($_POST["password"]);
+            $email = test_input($_POST["email"]);
+            // check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Invalid email format";
+            }
+        }
+
+        if (empty($_POST["website"])) {
+            $website = "";
+        } else {
+            $website = test_input($_POST["website"]);
+            // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
+                $websiteErr = "Invalid URL";
+            }
+        }
+
+        if (empty($_POST["comment"])) {
+            $comment = "";
+        } else {
+            $comment = test_input($_POST["comment"]);
+        }
+
+        if (empty($_POST["gender"])) {
+            $genderErr = "Gender is required";
+        } else {
+            $gender = test_input($_POST["gender"]);
         }
     }
 
+    //removes redundant html characters
     function test_input($data)
     {
         $data = trim($data);
@@ -41,41 +71,44 @@
         $data = htmlspecialchars($data);
         return $data;
     }
-
     ?>
 
-
-    <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
-        <div class="container">
-            <div class="card border border-dark">
-                <div>
-                    <div class="card-body">
-                        <p class="fs-5">Sign into your account</p>
-                        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                            <div class="form-group">
-                                <label for="text">User Name</label>
-                                <input type="text" name="userName" id="userName" class="form-control" placeholder="User Name">
-                                <span class="error">* <?php echo $userNameErr; ?> </span>
-                            </div>
-                            <div class="form-group mb-4">
-                                <label for="password" class="sr-only">Password</label>
-                                <input type="password" name="password" id="password" class="form-control" placeholder="***********">
-                                <span class="error">* <?php echo $passwordErr; ?> </span>
-                            </div>
-                            <input name="login" id="login" class="btn btn-block login-btn mb-4 border border-primary" type="submit">
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
+    <h2>PHP Form Validation Example</h2>
+    <p><span class="error">* required field</span></p>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <! –– validates all form input fields ––>
+        Name: <input type="text" name="name" value="<?php echo $name; ?>">
+        <span class="error">* <?php echo $nameErr; ?></span>
+        <br><br>
+        E-mail: <input type="text" name="email" value="<?php echo $email; ?>">
+        <span class="error">* <?php echo $emailErr; ?></span>
+        <br><br>
+        Website: <input type="text" name="website" value="<?php echo $website; ?>">
+        <span class="error"><?php echo $websiteErr; ?></span>
+        <br><br>
+        Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment; ?></textarea>
+        <br><br>
+        Gender:
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "female") echo "checked"; ?> value="female">Female
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "male") echo "checked"; ?> value="male">Male
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "other") echo "checked"; ?> value="other">Other
+        <span class="error">* <?php echo $genderErr; ?></span>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
 
     <?php
     echo "<h2>Your Input:</h2>";
-    echo $userName;
+    //accesses all input values by using their variable names and prints it to the webpage
+    echo $name;
     echo "<br>";
-    echo $password;
+    echo $email;
     echo "<br>";
+    echo $website;
+    echo "<br>";
+    echo $comment;
+    echo "<br>";
+    echo $gender;
     ?>
 
 </body>
